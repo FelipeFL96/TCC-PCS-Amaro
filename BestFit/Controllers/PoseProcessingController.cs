13c22b89e7cc13c94afbddd8bd9780578a18bfbe
AzambujaD <daniel.azambuja@usp.br>
2019-06-02 18:53:07 -0300

    adding bestfit project

---
+++ BestFit/Controllers/PoseProcessingController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BestFit.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PoseProcessingController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2", "value3"};
+    
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+    }
+}
---
+++ BestFit/Models/OpenPoseWrapper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BestFitAPIService.Model
+{
+    public class OpenPoseWrapper
+    {
+        public OpenPoseWrapper()
+        {
+
+        }
+    }
+}
